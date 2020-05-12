c1d3fc0b5fd08d6eefc9b6e4ad76c8aa8e357168
ChungYi347 <chungyi347@gmail.com>
2020-05-13 00:49:40 +0900

    GA

--- js/index.js
+++ js/index.js
@@ -153,15 +153,7 @@ $(function() {
   $('#contact-form').submit(function(e) {
     e.preventDefault();
 
-      $.ajax({
-          url: "https://formspree.io/xnqqdegq",
-          method: "POST",
-          data: { message: $('form').serialize() },
-          dataType: "json"
-      }).done(function(response) {
-          $('#success').addClass('expand');
-          $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-      });
+    
   });
 
   $('#close').click(function() {
